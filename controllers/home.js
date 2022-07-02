8ca8cb262d3860c43616a2f97ca2aa01bc578ed9
reaper6947 <mo47da@gmail.com>
2022-07-03 00:27:10 +0600

    views created

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
-    res.send("home page")
+    res.render("index")
 
 }
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config({ path: "./config/.env" });
+const cookieParser = require('cookie-parser')
+const path = require("path")
 const PORT = process.env.PORT || 3000;
-const DB = require("./config/db")
-DB()
+require("./config/db")()
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/public/views"));
 
-const register_route = require("./routes/registerRoute");
-const home = require("./routes/homeRoute")
-app.use("/api/", register_route);
-app.use("/",home)
+
+const registerRoute = require("./routes/registerRoute");
+const homeRoute = require("./routes/homeRoute")
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cookieParser(process.env.SECRET_SALT));
+
+app.use("/api/", registerRoute);
+app.use("/",homeRoute)
 app.listen(PORT, () => console.log(`server on ${PORT}`));
